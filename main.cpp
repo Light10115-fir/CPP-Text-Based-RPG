9d6fb3d12388ebe0561f274cc2258b69d12f0a0e
Light10115 <light10115.free@gmail.com>
2026-07-06 18:56:19 +0300

    Player can now check inventory, fight zombie, quit game, explore.

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ class Items
 	public:	
 };
 
-// Create child Weapons class
+// Create child Weapons class, Items parent
 class Weapon : public Items
 {
 	public:
@@ -28,7 +28,7 @@ class Weapon : public Items
 		string name;
 };
 
-// Create child MiscItems class
+// Create child MiscItems class, Items parent
 class MiscItems : public Items
 {
 	public:
@@ -38,69 +38,127 @@ class MiscItems : public Items
 
 int main()
 {
-	// Create sword weapon
-	Weapon sword;
-	// Give sword damage value
-	sword.damage = 5;
-	// Give sword name
-	sword.name = "Sword";
-	// Create bandage item
-	MiscItems bandage;
-	// Give bandage name
-	bandage.name = "Bandage";
-	// Give sword heal value
-	bandage.healValue = 10;
-	// Create inventory
-	string inventory[5] = {sword.name, bandage.name};
-	// Create int to sort through inventory
-	int sortInv = sizeof(inventory) / sizeof(inventory[0]);
-	// Start game
-	cout << "What would you like to do?  \n Explore (1)	Check inventory(2)	" ;
-	// Make playerChoice variable
-	string playerChoice = "2";
-	// See what player wants to do
-	cin >> playerChoice;
-	// debugging
-	cout << "playerChoice: " << playerChoice << "\n";
-	// See if logic compiler chose
-	int ifLogic;
-	if(playerChoice == "Explore" || "1")
-	{
-		// If logic variable is 1
-		ifLogic = 1;
-		// random number decides if the
-		// player encounters an enemy or not
-		int randomNum = randnum(2);
-		
-		bool enemyEncounter;
-		
-		int numberEnemies;
-		if(randomNum == 1)
+	// start game
+	//while(true){
+		// Create sword weapon
+		Weapon sword;
+		// Give sword damage value
+		sword.damage = 5;
+		// Give sword name
+		sword.name = "Sword";
+		// Create fists weapon
+		Weapon fists;
+		// Give fists name
+		fists.name = "Fists";
+		// Give fists damage
+		fists.damage = 3;
+		// Create bandage item
+		MiscItems bandage;
+		// Give bandage name
+		bandage.name = "Bandage";
+		// Give bandage heal value
+		bandage.healValue = 7;
+		// Create inventory
+		string inventory[5] = {bandage.name, fists.name};
+		// Create int to sort through inventory
+		int sortInv = sizeof(inventory) / sizeof(inventory[0]);
+		// Create zombie
+		Being zombie;
+		// Give zombie name
+		zombie.name = "Zombie";
+		// Give zombie HP
+		zombie.hp = 7;
+		// Give zombie damage
+		zombie.damage = 4;
+		// Create player
+		Being player;
+		// Give player name
+		player.name = "Player";
+		// Give player HP
+		player.hp = 10;
+		// Start game
+		while(player.hp >0)
 		{
-			// player encounters enemy/enemies
-			enemyEncounter = true;
+			cout << "What would you like to do?  \n Explore (1)	Check inventory (2)	Quit (3) \n" ;
+			// Make playerChoice variable
+			string playerChoice = "2";
+			// See what player wants to do
+			cin >> playerChoice;
+			// debugging
+			cout << "playerChoice: " << playerChoice << "\n";
+			// See if logic compiler chose
+			int ifLogic;
+			if(playerChoice == "Explore" || playerChoice == "1")
+			{
+				// If logic variable is 1
+				ifLogic = 1;
+				// random number decides if the
+				// player encounters an enemy or not
+				int randomNum = randnum(2);
 
-			// number of enemies decided by random number
-			numberEnemies = randnum(20);
-		}
-		else
-		{
-			// player doesnt encounter an enemy/enemies
-			enemyEncounter = false;
-		}
-		// debugging
-		cout << "randomNum: " << randomNum << "\n" << "enemyEncounter: " << enemyEncounter << "\n";
-		cout << "If logic: " << ifLogic << "\n";
-	}
-	else if (playerChoice == "Check inventory" || "2")
-	{
-		// ifLogic variable is 2
-		ifLogic = 2;
-		for(int i = 0; i >= sortInv; i++)
-		{
-			cout << inventory[i] << ' ';
-		}
-		cout << "\n";
-		cout << "If logic: " << ifLogic << "\n";
+				// enemy encounter boolean	
+				bool enemyEncounter;
+		
+				int numberEnemies;
+				if(randomNum == 1)
+				{
+					// player acts
+					string playerAct;
+					// number of enemies decided by random number
+					numberEnemies = randnum(20);
+
+					// tell player they encountered enemy
+					// and ask them what they wanna do
+					cout << "You encountered a zombie! \n";
+					while(zombie.hp > 0)
+					{
+						cout <<	"What do you wanna do?	attack (1)	heal (2)	block (3) \n";
+			
+						// take playerAct response
+						cin >> playerAct;
+						if (playerAct == "attack" || playerAct == "1")
+						{
+							// zombie takes damage
+							zombie.hp = zombie.hp - fists.damage;
+
+							// tell player the results of their attack
+							cout << "You attacked! Zombie took " << fists.damage << " damage and has " << zombie.hp << " health. \n";
+
+							// player takes damage from zombie attack
+							player.hp = player.hp - zombie.damage;
+
+							// tell player the results of the zombie's attack
+							cout << "Zombie attacked you! You took " << zombie.damage << " damage and have " << player.hp << " health. \n";
+						}
+					}
+				}
+				else
+				{
+					// player doesnt encounter an enemy/enemies
+					enemyEncounter = false;
+				}
+				// debugging
+				cout << "randomNum: " << randomNum << "\n" << "enemyEncounter: " << enemyEncounter << "\n";
+				cout << "If logic: " << ifLogic << "\n";
+			}
+			else if (playerChoice == "Check inventory" || playerChoice == "2")
+			{
+				// ifLogic variable is 2
+				ifLogic = 2;
+				for(string i : inventory)
+				{
+					// print each item in inventory
+					cout << i << ' ';
+				}
+				cout << "\n";
+				cout << "If logic: " << ifLogic << "\n";
+			}
+			else if (playerChoice == "Quit game" || playerChoice == "3")
+			{
+				ifLogic = 3;
+				cout << "If logic: " << ifLogic << "\n Quitting... \n";
+				return 0;
+			}
+		//}
 	}
 }
